f7985d3273f59e5f06eda44992198d2ffa398eb1
WarDenToC <amrithkong20011@gmail.com>
2025-10-26 15:04:43 +1300

    Refactoring
    Refactoring small issues and bugs for both classes, StatTracker is now the logger used to write into database since it has all the raw data

--- DataBaseConn.java
+++ DataBaseConn.java
@@ -1,14 +1,11 @@
 package com.mycompany.amrithkongv2;
 
-/*
- * The programs are designed for PDC paper
- */
-
-
 import java.sql.*;
+import java.time.LocalDateTime;
 
 
-public final class  DataBaseConn{
+public final class  DataBaseConn
+{
 
 
     private static final String USER_NAME = ""; //your DB username
@@ -19,13 +16,10 @@ public final class  DataBaseConn{
 
     public DataBaseConn() 
     {
-        establishConnection();
-    }
-
-    public static void main(String[] args) 
-    {
-        DataBaseConn dbManager = new DataBaseConn();
-        System.out.println(dbManager.getConnection());
+        //establishConnection();
+        createMemorialTable();
+        createHistoryTable();
+        createRecentGameTable();
     }
 
     public Connection getConnection() 
@@ -34,6 +28,7 @@ public final class  DataBaseConn{
     }
 
     //Establish connection
+    /*
     public void establishConnection() 
     {
         if (this.conn == null) {
@@ -52,6 +47,133 @@ public final class  DataBaseConn{
             try {
                 conn.close();
             } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }*/
+    
+    
+    ///////////////
+    public void createMemorialTable() // Creates a table that logs new players, does not update existing player or will add old players
+    {
+        try(Connection conn = DriverManager.getConnection(URL);Statement smt = conn.createStatement())
+        {
+            smt.executeUpdate("CREATE TABLE MemorialTable (PlayerName VARCHAR(50) PRIMARY KEY)");
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Something wrong with the memorial table");
+        }
+    }
+    
+    public void addNewPlayers(String name) //method for memorial table
+    {
+        String insertion = "INSERT INTO MemorialTable (PlayerName) VALUES (?)";
+        try(Connection conn = DriverManager.getConnection(URL); PreparedStatement ps = conn.prepareStatement(insertion))
+        {   
+            ps.setString(1, name);
+            ps.executeUpdate();
+        }
+        catch(SQLException e)
+        {
+            if ("23505".equals(e.getSQLState())) //handles duplicate keys in the exception 
+            {
+                System.out.println("Player already exists: " + name);
+            } 
+        }
+        
+    }
+    //////////////////
+    
+    
+    /////////
+    public void createHistoryTable()
+    {
+        try(Connection conn = DriverManager.getConnection(URL);Statement smt = conn.createStatement())
+        {
+            smt.executeUpdate("""
+                CREATE TABLE HistoricalTable (
+                              PlayerName VARCHAR(50) PRIMARY KEY,
+                              GameDate TIMESTAMP,
+                              SelfShot INT,
+                              TurnsSurvived INT
+                                            )
+                              """);
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Something wrong with the Historical Table");
+        }
+        
+    }
+    
+    public void logHistory(String name, LocalDateTime gameDate, int SelfShot, int TurnsSurvived) //method for memorial table
+    {
+        String insertion = "INSERT INTO HistoricalTable (PlayerName, gameDate, SelfShot, TurnsSurvived) VALUES (?, ?, ?, ?)";
+        try(Connection conn = DriverManager.getConnection(URL); PreparedStatement ps = conn.prepareStatement(insertion))
+        {
+            ps.setString(1, name);
+            ps.setTimestamp(2, Timestamp.valueOf(gameDate));
+            ps.setInt(3, SelfShot);
+            ps.setInt(4, TurnsSurvived);
+            ps.executeUpdate();
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Something went wrong with the LogHistory, check it out"); 
+        }
+        
+    }
+    ///////////
+    
+    public void createRecentGameTable()
+    {
+        try(Connection conn = DriverManager.getConnection(URL);Statement smt = conn.createStatement())
+        {
+            smt.executeUpdate("""
+                CREATE TABLE RecentGameTable (
+                              PlayerName VARCHAR(50) PRIMARY KEY,
+                              GameDate TIMESTAMP,
+                              Status BOOLEAN
+                                            )
+                              """);
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Something wrong with the RecentGameTable");
+        }
+    }
+    
+    public void logRecentGame(String name, LocalDateTime gameDate, boolean survived) 
+    {
+        String rewrite = "DELETE FROM RecentGameTable";
+        String insertion = "INSERT INTO RecentGameTable (PlayerName, gameDate, survived) VALUES (?, ?, ?)";
+        try(Connection conn = DriverManager.getConnection(URL); PreparedStatement ps = conn.prepareStatement(insertion); Statement smt = conn.createStatement();)
+        {
+            smt.executeUpdate(rewrite); //rewrite the whole table so we can only include the previous game
+            ps.setString(1, name);
+            ps.setTimestamp(2, Timestamp.valueOf(gameDate));
+            ps.setBoolean(3, survived);
+            ps.executeUpdate();
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Something went wrong with the LogHistory, check it out"); 
+        }
+        
+    }
+    ///////////
+    
+    public void closeConnections() 
+    {
+        if (conn != null) 
+        {
+            try 
+            {
+                conn.close();
+            } 
+            catch (SQLException ex) 
+            {
                 System.out.println(ex.getMessage());
             }
         }
---
+++ StatTracker.java
@@ -0,0 +1,186 @@
+package com.mycompany.amrithkongv2;
+import java.time.LocalDateTime; 
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class StatTracker 
+{
+    HashMap<String, Integer> selfShot = new HashMap<String, Integer>();
+    HashMap<String, Integer> turnsSurvived = new HashMap<String, Integer>();
+    RevolverTemplate gun;
+    private Logger statLogger;
+    ArrayList<Player> players;
+    Scanner scanner = new Scanner(System.in);
+    LogReader reader = new LogReader();
+    private DataBaseConn db;
+    
+    public StatTracker(Logger log, ArrayList<Player> players)
+    {
+        this.statLogger = log;
+        this.players = players;
+        this.db = new DataBaseConn();
+    }
+    
+    public void setupStats() // This sets up the: key would be the names, the value would be the amount of turns survived and shooting self. Using hashmap 
+    {
+        selfShot.put(players.get(0).getName(), 0);
+        selfShot.put(players.get(1).getName(), 0);
+        turnsSurvived.put(players.get(0).getName(), 0);
+        turnsSurvived.put(players.get(1).getName(), 0);
+    }
+    
+    public void setGun(RevolverTemplate gun) // it was hard for me to make a constructor for this. So I just the setter method instead since it was easier
+    {
+        this.gun = gun;
+    }
+    
+    public void incrementSelfShot(String playerName)
+    {
+        selfShot.put(playerName, selfShot.getOrDefault(playerName, 0) + 1); // chatGpt helped me with the logic since I didn't know how to update an existed key with value
+    }
+    
+    public void incrementTurnsSurvived(String playerName) //plus one whenever a player survives the turn 
+    {
+        turnsSurvived.put(playerName, turnsSurvived.getOrDefault(playerName, 0) + 1); 
+    }
+    
+    public HashMap getSelfShots() //returns whatever is in the HashMap of self-shooting
+    {
+        return selfShot;
+    }
+    
+    public HashMap getTurnsSurvived() //returns whatever is in the HashMap of turnsSurvived 
+    {
+        return turnsSurvived;
+    }
+    
+    
+    public void PrintStats() 
+    {
+        LocalDateTime currentDate = LocalDateTime.now();
+        DateTimeFormatter formattedDate = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String date = currentDate.format(formattedDate);
+        
+        StringBuilder sb = new StringBuilder(); // for this I was using println to print it, but then I had to re-code this for both printing and logging so I asked chat to rewrite this, manual work
+               
+        db.logHistory(players.get(0).getName(), currentDate, selfShot.get(players.get(0)), turnsSurvived.get(players.get(0)));
+        db.logHistory(players.get(1).getName(), currentDate, selfShot.get(players.get(1)), turnsSurvived.get(players.get(1)));
+        
+        sb.append("\n=======Endgame=======\n");
+        sb.append("Date: ").append(date).append("\n");
+        sb.append("=======Players=======\n");
+        sb.append("Player 1: ").append(players.get(0).getName()).append("\n");
+        sb.append("Player 2: ").append(players.get(1).getName()).append("\n");
+        sb.append("=======Outcome=======\n");
+        if(!players.get(0).getAlive()) 
+        {
+            sb.append("Winner: ").append(players.get(1).getName()).append("\n");
+            sb.append("Loser: ").append(players.get(0).getName()).append("\n");
+    
+        } 
+        else 
+        {
+            sb.append("Winner: ").append(players.get(0).getName()).append("\n");
+            sb.append("Loser: ").append(players.get(1).getName()).append("\n");
+        }
+
+        sb.append("=============Technical Stats=============\n");
+        sb.append("Amount of time each player shot themselves:\n").append(selfShot).append("\n");
+        sb.append("Amount of time each player survives a turn:\n").append(turnsSurvived).append("\n");
+        sb.append("Gun chosen: ").append(gun.gunModel).append("\n");
+
+        
+    
+        System.out.println(sb.toString()); // print the string builder
+
+        // Log to file
+        if(statLogger != null) // Since I was passing our logger through multiple classes, I had to use this to test for null. 
+            //Basically, hides the null error, if the files has been written that mean it works, if it's empty that means this method was never aclled
+        {
+            statLogger.LogResult(sb.toString());
+        }
+    }
+    
+    public void PrintPreviousGame() // same as above but I am cutting out the technical part 
+    {
+        LocalDateTime currentDate = LocalDateTime.now();
+        DateTimeFormatter formattedDate = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String date = currentDate.format(formattedDate);
+        
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("=======Endgame=======\n");
+        sb.append("Date: ").append(date).append("\n");
+        sb.append("=======Players=======\n");
+        sb.append("Player 1: ").append(players.get(0).getName()).append("\n");
+        sb.append("Player 2: ").append(players.get(1).getName()).append("\n");
+        sb.append("=======Outcome=======\n");
+        if(!players.get(0).getAlive()) 
+        {
+            sb.append("Winner: ").append(players.get(1).getName()).append("\n");
+            sb.append("Loser: ").append(players.get(0).getName()).append("\n");
+    
+        } 
+        else 
+        {
+            sb.append("Winner: ").append(players.get(0).getName()).append("\n");
+            sb.append("Loser: ").append(players.get(1).getName()).append("\n");
+        }
+        
+        db.logRecentGame(players.get(0).getName(), currentDate, players.get(0).getAlive());
+        db.logRecentGame(players.get(1).getName(), currentDate, players.get(1).getAlive());
+        
+        System.out.println("Would you like to check the previous game match? (Y/N)");
+        String input = scanner.nextLine();
+        
+        while(!input.equalsIgnoreCase("N") && !input.equalsIgnoreCase("Y"))
+        {
+            System.out.println("Please enter the given options(Y/N)");
+            input = scanner.nextLine();
+        }
+        
+        if(input.equalsIgnoreCase("Y")) // if they say yes then we will read the previous game stats and print it to the console thenn we will overwrite that file 
+        {
+            if(statLogger != null) 
+            {
+                reader.readPreviousGame();
+                statLogger.LogRecentGame(sb.toString()); //pass the whole string ot the logger after reading the previous game before updating it 
+            }   
+        }
+        else
+        {
+            if(statLogger != null) 
+            {
+                statLogger.LogRecentGame(sb.toString());
+            }
+        }
+    }
+    
+    public void printPreviousPlayer() //reads all the players that have played this game 
+    {
+        db.addNewPlayers(players.get(0).getName());
+        db.addNewPlayers(players.get(1).getName());
+        
+        System.out.println("Would you like to check the players that have played this game?(Y/N)");
+        String input = scanner.nextLine();
+        
+        while(!input.equalsIgnoreCase("N") && !input.equalsIgnoreCase("Y"))
+        {
+            System.out.println("Please enter the given options(Y/N)");
+            input = scanner.nextLine();
+        }
+        
+        if(input.equalsIgnoreCase("Y"))
+        {
+            reader.readParticipatedPlayer(players, true);
+        }
+        else
+        {
+            reader.readParticipatedPlayer(players, false);   
+        }
+        
+        
+        
+    }
+    
+}
